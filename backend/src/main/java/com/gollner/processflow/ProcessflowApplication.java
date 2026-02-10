4f2e5b482553a06f6ea663d3552bff4ea89f01da
Eduardo Sousa Gollner <dudugollner@gmail.com>
2026-02-09 22:07:39 -0300

    Criando projeto Spring (backend)

---
+++ backend/src/main/java/com/gollner/processflow/ProcessflowApplication.java
@@ -0,0 +1,13 @@
+package com.gollner.processflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessflowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gollner/processflow/ProcessflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gollner.processflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
